610e4ee58bc7ca1a61ec9a2cffff64f6b12df442
tonybeyond <42512239+tonybeyond@users.noreply.github.com>
2026-02-09 08:36:36 +0100

    Update Neovim installation script for ARM64 package

--- niri2.sh
+++ niri2.sh
@@ -86,7 +86,7 @@ fi
 cd "$HOME/neovim-build"
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build && cpack -G DEB
-sudo dpkg -i nvim-linux64.deb
+sudo dpkg -i $HOME/neovim-build/build/nvim-linux-arm64.deb
 
 # Install Kickstart config
 if [ ! -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvim" ]; then
